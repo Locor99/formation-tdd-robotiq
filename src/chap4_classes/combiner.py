8fb57d4561b1467baf7c758ddade4b3c09a64a95
loucorm <l.cormier@robotiq.com>
2023-05-18 17:29:02 -0400

    Refactor: Added test setup, cleaned squash

--- src/chap4_classes/combiner.py
+++ src/chap4_classes/combiner.py
@@ -2,15 +2,13 @@ class Combiner:
     def squash(self, array):
         # return [None, None, None, None]
 
-        while True:
+        tileWasMoved = True
+        while tileWasMoved:
             tileWasMoved = False
             for i in range(1, len(array)):
                 if array[i-1] is None and array[i] is not None:
                     array[i-1] = array[i]
                     array[i] = None
                     tileWasMoved = True
-
-            if not tileWasMoved:
-                break
 
         return array
--- tests/chap4_classes/test_2048_combiner.py
+++ tests/chap4_classes/test_2048_combiner.py
@@ -3,16 +3,17 @@ from src.chap4_classes.combiner import Combiner
 
 
 class TestCombiner(TestCase):
+    def setUp(self) -> None:
+        self.aCombiner = Combiner()
+
     def test_an_empty_array_should_be_squashed_into_an_empty_array(self):
-        aCombiner = Combiner()
         array = [None, None, None, None]
-        squashedArray = aCombiner.squash(array)
+        squashedArray = self.aCombiner.squash(array)
 
         self.assertEqual([None, None, None, None], squashedArray)
 
     def test_an_array_with_one_element_on_the_right_should_be_squashed_into_an_array_with_the_element_on_the_left(self):
-        aCombiner = Combiner()
         array = [None, None, None, 2]
-        squashedArray = aCombiner.squash(array)
+        squashedArray = self.aCombiner.squash(array)
 
         self.assertEqual([2, None, None, None], squashedArray)
