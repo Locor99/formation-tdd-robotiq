83ab56e6f64d14f8a6649f494a47614d4b5aafd3
loucorm <l.cormier@robotiq.com>
2023-05-18 16:01:05 -0400

    failing test

--- tests/chap4_classes/test_2048_combiner.py
+++ tests/chap4_classes/test_2048_combiner.py
@@ -3,5 +3,10 @@ from src.chap4_classes.combiner import Combiner
 
 
 class TestCombiner(TestCase):
-    def test_fail(self):
-        self.assertTrue(False)
+    def test_an_empty_array_should_be_squashed_in_an_empty_array(self):
+        aCombiner = Combiner()
+        array = [None, None, None, None]
+        squashedArray = aCombiner.squash(array)
+
+        expectedSquashedArray = [None, None, None, None]
+        self.assertEqual(expectedSquashedArray, squashedArray)
