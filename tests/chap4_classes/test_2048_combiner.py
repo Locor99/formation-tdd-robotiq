8ec2da1a99fbaa00e9dd96f652c37f82f283c881
loucorm <l.cormier@robotiq.com>
2023-05-18 17:08:01 -0400

    test succeeds

--- tests/chap4_classes/test_2048_combiner.py
+++ tests/chap4_classes/test_2048_combiner.py
@@ -8,5 +8,11 @@ class TestCombiner(TestCase):
         array = [None, None, None, None]
         squashedArray = aCombiner.squash(array)
 
-        expectedSquashedArray = [None, None, None, None]
-        self.assertEqual(expectedSquashedArray, squashedArray)
+        self.assertEqual([None, None, None, None], squashedArray)
+
+    def test_an_array_with_one_element_on_the_right_should_be_squashed_into_an_array_with_the_element_on_the_left(self):
+        aCombiner = Combiner()
+        array = [None, None, None, 2]
+        squashedArray = aCombiner.squash(array)
+
+        self.assertEqual([2, None, None, None], squashedArray)
