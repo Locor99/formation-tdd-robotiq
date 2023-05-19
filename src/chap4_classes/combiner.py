585258e1311d752271f53c424f52bbe3be35367c
loucorm <l.cormier@robotiq.com>
2023-05-18 22:29:18 -0400

    Rework: Added tile_is_empty method

--- src/chap4_classes/combiner.py
+++ src/chap4_classes/combiner.py
@@ -1,18 +1,24 @@
 class Combiner:
+    def __init__(self):
+        self.activeArray = []
     def squash(self, array):
+        self.activeArray = array
         # return [None, None, None, None]
 
         tileWasMoved = True
         while tileWasMoved:
             tileWasMoved = False
             for i in range(1, len(array)):
-                if array[i-1] is None and array[i] is not None:
-                    array[i-1] = array[i]
-                    array[i] = None
+                if self._tile_is_empty(i-1) and not self._tile_is_empty(i):
+                    self.activeArray[i-1] = self.activeArray[i]
+                    self.activeArray[i] = None
                     tileWasMoved = True
 
-                elif array[i-1] is not None and array[i-1] == array[i]:
-                    array[i-1] *= 2
-                    array[i] = None
+                elif not self._tile_is_empty(i-1) and self.activeArray[i-1] == self.activeArray[i]:
+                    self.activeArray[i-1] *= 2
+                    self.activeArray[i] = None
 
         return array
+
+    def _tile_is_empty(self, index):
+        return self.activeArray[index] is None
